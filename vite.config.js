09c9e56355d4d9b6c8a91f4616e980f81128ed7d
Preetham <preethamb97@gmail.com>
2026-05-01 17:03:13 +0530

    sanity

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// GitHub project page: https://<user>.github.io/preetham-portfolio/
 export default defineConfig({
+  base: "/preetham-portfolio/",
   plugins: [react()],
 })
